1dacb628f8ecba6158ac1700d189ee9e329e64aa
Nolger Rodriguez <nolgerluisrodriguezmedina@gmail.com>
2022-12-28 15:37:30 -0500

    Card4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Card1 } from "./Cards/Card1"
 import Card2 from "./Cards/Card2"
 import Card3 from "./Cards/Card3"
+import Card4 from "./Cards/Card4"
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Card1 />
       <Card2 />
       <Card3 />
+      <Card4 />
     </div>
   )
 }
---
+++ src/Cards/Card4.jsx
@@ -0,0 +1,18 @@
+import img from '../assets/JuiceOrange.jpg'
+const Card4 = () => {
+ return (
+    <div className='m-8'>
+        <div className='bg-orange-400 w-96 rounded-xl flex'>
+            <img src={img} className="w-52 rounded-l-2xl" />
+            <div className='p-4 w-48'>
+                <h1 className='text-center text-2xl font-bold text-white mb-3'>Orange</h1>
+                <span className='text-sm text-orange-900'>Cada vez que se hace referencia al jugo de naranja no podemos dejar de imaginar un delicioso desayuno para empezar el día con toda la energía posible. </span>
+                <div className='flex justify-center mt-2'>
+                    <button className='p-2 bg-white rounded-xl text-orange-400 font-bold mt-4'>Comprar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+ )
+}
+export default (Card4)
