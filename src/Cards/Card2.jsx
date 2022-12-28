5e57b2044e241ac549d2ce341d2ca639ec8e6eb0
Nolger Rodriguez <nolgerluisrodriguezmedina@gmail.com>
2022-12-27 21:01:36 -0500

    3 card

--- src/Cards/Card2.jsx
+++ src/Cards/Card2.jsx
@@ -5,12 +5,16 @@ const Card2 = () => {
       <div className="bg-slate-300 w-60 h-80 rounded-xl">
         <div className="w-full p-2 relative">
           <img src={img} className="w-44 rounded-full mx-auto relative" />
-          <span className="absolute top-1 right-1 mt-6 mr-8 bg-green-600 rounded-xl p-1 text-sm text-white">On</span>
+          <span className="absolute top-1 right-1 mt-6 mr-8 bg-green-600 rounded-xl p-1 text-sm text-white">
+            On
+          </span>
         </div>
         <h1 className="text-center text-2xl font-semibold mt-4">Nolger R</h1>
         <h6 className="text-center font-extralight text-sm">Usuario nuevo</h6>
         <div className="flex">
-          <button className="w-1/2 p-2 rounded-xl text-white font-bold mx-auto mt-8 bg-blue-400">Seguir</button>
+          <button className="w-1/2 p-2 rounded-xl text-white font-bold mx-auto mt-8 bg-blue-400">
+            Seguir
+          </button>
         </div>
       </div>
     </div>
--- src/Cards/Card3.jsx
+++ src/Cards/Card3.jsx
@@ -1,18 +1,24 @@
-import img from '../assets/Orange.jpg'
+import img from "../assets/Orange.jpg";
 const Card3 = () => {
-    return(
+  return (
+    <div>
+      <div className="bg-orange-400 w-80 h-96 m-8 rounded-xl">
         <div>
-            <div className="bg-orange-400 w-80 h-96 m-8 rounded-xl">
-                <div>
-                    <img src={img} className="w-80 rounded-t-xl" />
-                </div>
-                <div className='p-2'>
-                    <h1 className="text-2xl font-bold text-white">Orange</h1>
-                    <p className="text-sm font-extralight text-orange-800">Orange is the color of the orange fruit.</p>
-                </div>
-                <div className='flex justify-end mt-5 mr-2'><button className='p-2 bg-white rounded-xl text-orange-400 font-bold'>Comprar</button></div>
-            </div>
+          <img src={img} className="w-80 rounded-t-xl" />
         </div>
-    )
-}
-export default (Card3)
+        <div className="p-2">
+          <h1 className="text-2xl font-bold text-white">Orange</h1>
+          <p className="text-sm font-extralight text-orange-800">
+            Orange is the color of the orange fruit.
+          </p>
+        </div>
+        <div className="flex justify-end mt-5 mr-2">
+          <button className="p-2 bg-white rounded-xl text-orange-400 font-bold">
+            Comprar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Card3;
